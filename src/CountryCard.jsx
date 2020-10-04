ae78a50751914c856908f49c8a1a9c594cfe0e0f
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-10-04 20:37:45 +0100

    initial files are created

---
+++ src/CountryCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CountryFlag from "./CountryFlag";
+import CountryInfo from "./CountryInfo";
+
+const CountryCard = () => {
+  return (
+    <div class="card mb-3">
+      <div class="row no-gutters">
+        <CountryFlag />
+        <CountryInfo />
+      </div>
+    </div>
+  );
+};
+
+export default CountryCard;
---
+++ src/CountryFlag.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const  CountryFlag =()=>{
+     return (
+       <div class="col-md-4">
+         <img src="..." class="card-img" alt="..." />
+       </div>
+     );
+}       
+
+
+export default CountryFlag;
