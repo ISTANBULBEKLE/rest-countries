5861b74bcb5c98de40c61340779df3a5b9bea82b
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-10-05 00:09:00 +0100

    level-1 is done

--- src/App.js
+++ src/App.js
@@ -8,40 +8,48 @@ function App() {
     <div className="App">
       <h1>Rest Countries</h1>
       {Countries.map((c) => (
-        <div className="card-container">
-          <div class="row no-gutters col-md-4">
-            <img src={c.flag} className="card-img" alt="country-flag" />
-          </div>
-          <h2> {c.name}</h2>
-          <div className="card-body info-container">
-            <p class="card-text">
-              <small className="text-muted">Native Name: {c.nativeName}</small>
-            </p>
-            <p>
-              <small className="text-muted">Population: {c.population}</small>
-            </p>
-            <p>
-              <small className="text-muted">Region: {c.region}</small>
-            </p>
-            <p>
-              <small className="text-muted">Capitol: {c.capital}</small>
-            </p>
-            <p>
-              <small className="text-muted">
-                Top Level Domain: {c.topLevelDomain}
-              </small>
-            </p>
-            <p>
-              <small className="text-muted">
-                Currencies: {c.currencies.name}
-              </small>
-            </p>
-            <p>
-              <small className="text-muted">Languages: </small>
-            </p>
-            <p>
-              <small className="text-muted">Border Countries:</small>
-            </p>
+        <div className="card mb-3 " >
+          <div className="row no-gutters container">
+            <div className="col-md-4">
+              <img src={c.flag} className="card-img" alt="country-flag" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h2 className="card-title"> {c.name}</h2>
+                <p class="card-text">
+                  <small className="text-muted">
+                    Native Name: {c.nativeName}
+                  </small>
+                </p>
+                <p>
+                  <small className="text-muted">
+                    Population: {c.population}
+                  </small>
+                </p>
+                <p>
+                  <small className="text-muted">Region: {c.region}</small>
+                </p>
+                <p>
+                  <small className="text-muted">Capitol: {c.capital}</small>
+                </p>
+                <p>
+                  <small className="text-muted">
+                    Top Level Domain: {c.topLevelDomain}
+                  </small>
+                </p>
+                <p>
+                  <small className="text-muted">
+                    Currencies: {c.currencies.name}
+                  </small>
+                </p>
+                <p>
+                  <small className="text-muted">Languages: </small>
+                </p>
+                <p>
+                  <small className="text-muted">Border Countries:</small>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       ))}
